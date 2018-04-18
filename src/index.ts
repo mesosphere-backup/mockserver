a4f37472cbae120f5617feb1b0a42772d0c49c19
Daniel Schmidt <dschmidt@mesosphere.com>
2018-04-18 12:08:30 +0200

    refactor(proxy): catch errors for getPort

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,21 @@ import log from "npmlog";
 
 import server from "./server";
 import { getPort, getProxyHostPort } from "./config";
+
 let hostPort;
+let port;
+
 try {
   hostPort = getProxyHostPort();
+  port = getPort();
 } catch (e) {
   log.error("setup", e.message);
   process.exit(1);
 }
 
-server(getPort(), hostPort);
+try {
+  server(port, hostPort);
+} catch (e) {
+  log.error("run", e.message);
+  process.exit(1);
+}
