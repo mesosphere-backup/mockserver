90163848a9dab4b22a08496694a4be48b4aa6b84
Daniel Schmidt <dschmidt@mesosphere.com>
2018-04-19 16:19:14 +0200

    chore(tooling): start mock server for every test
    This allows us to execute the tests in parallel

--- __tests__/proxy-xhr-test.ts
+++ __tests__/proxy-xhr-test.ts
@@ -1,23 +1,11 @@
 jest.unmock("node-fetch");
-jest.unmock("express");
 
 import fetch from "node-fetch";
-import express from "express";
-import bodyparser from "body-parser";
+import createServer from "./helpers/createServer";
+import MockServerController from "./helpers/mockServerController";
 
-function createServer() {
-  return new Promise(resolve => {
-    const app = express();
-    app.use(bodyparser.json());
-
-    const server = app.listen(4241, () => {
-      resolve({ app, server });
-    });
-  });
-}
-
-function xhr(options = {}) {
-  return fetch("http://localhost:1111", {
+function xhr(port, options = {}) {
+  return fetch(`http://localhost:${port}`, {
     ...options,
     headers: {
       ...options.headers,
@@ -28,12 +16,17 @@ function xhr(options = {}) {
 
 describe("Proxy - XHR", () => {
   let s;
+  let msPort;
+  let msController;
 
   beforeEach(async () => {
     s = await createServer();
+    msController = new MockServerController(s.port);
+    msPort = await msController.start();
   });
 
   afterEach(() => {
+    msController.stop();
     s.server.close();
   });
 
@@ -48,27 +41,30 @@ describe("Proxy - XHR", () => {
     });
 
     it("forwards the request", async () => {
-      await xhr();
+      await xhr(msPort);
 
       expect(mockMiddleware).toHaveBeenCalled();
     });
 
     it("forwards the headers", async () => {
-      await xhr({ headers: { "my-header": "is-awesome" } });
+      await xhr(msPort, { headers: { "my-header": "is-awesome" } });
 
       const call = mockMiddleware.mock.calls[0][0];
       expect(call.headers["my-header"]).toBe("is-awesome");
     });
 
     it("forwards the method", async () => {
-      await xhr({ method: "POST" });
+      await xhr(msPort, { method: "POST" });
 
       const call = mockMiddleware.mock.calls[0][0];
       expect(call.method).toBe("POST");
     });
 
     it("forwards the body", async () => {
-      await xhr({ method: "POST", body: JSON.stringify({ foo: "bar" }) });
+      await xhr(msPort, {
+        method: "POST",
+        body: JSON.stringify({ foo: "bar" })
+      });
 
       const call = mockMiddleware.mock.calls[0][0];
       expect(call.body.foo).toBe("bar");
@@ -85,17 +81,17 @@ describe("Proxy - XHR", () => {
     });
 
     it("forwards the headers", async () => {
-      const response = await xhr();
+      const response = await xhr(msPort);
       expect(response.headers.get("x-b3-span-id")).toBe("my-tracing");
     });
 
     it("forwards the status code", async () => {
-      const response = await xhr();
+      const response = await xhr(msPort);
       expect(response.status).toBe(201);
     });
 
     it("forwards the body", async () => {
-      const response = await xhr().then(res => res.json());
+      const response = await xhr(msPort).then(res => res.json());
       expect(response.done).toBe(true);
     });
   });
